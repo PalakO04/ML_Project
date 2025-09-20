563b1800621b7ff774d37e1f7758874c446fe432
Palak Makhija <makhijapalak247@gmail.com>
2025-09-20 08:55:09 +0530

    Prediction pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -104,7 +104,7 @@ class ModelTrainer:
 
             predicted=best_model.predict(X_test)
             r2 = r2_score(y_test,predicted)
-            return r2_score
+            return r2
         except Exception as e:
             raise CustomException(e,sys)
             
